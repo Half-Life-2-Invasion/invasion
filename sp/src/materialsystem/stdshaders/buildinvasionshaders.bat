dc8c86e5ac3f47e2411358ac399df0a829ba98f4
95Navigator <marco_vale_1995@hotmail.com>
2026-09-18 17:42:02 +0100

    Addition of Valve's Source SDK 2013 MP fork of SCell555's ShaderCompiler
    - Added Valve's Source SDK 2013 MP fork of SCell555's ShaderCompiler;
    - Placed all shader .inc files in a new "include" folder, and updated the game_shader_dx9 VPC script to include it;
    - Removed all instances of D3DVERTEXTEXTURESAMPLER0 from the shaders, so they can be compiled with the new ShaderCompiler.

--- sp/src/materialsystem/stdshaders/buildinvasionshaders.bat
+++ sp/src/materialsystem/stdshaders/buildinvasionshaders.bat
@@ -43,9 +43,9 @@ set ARG_EXTRA=
 set dynamic_shaders=1
 %BUILD_SHADER% stdshader_dx9_20b		-game %GAMEDIR% -source %SOURCEDIR%
 set dynamic_shaders=
-%BUILD_SHADER% stdshader_dx9_30			-game %GAMEDIR% -source %SOURCEDIR% -dx9_30	-force30 
+%BUILD_SHADER% stdshader_dx9_30			-game %GAMEDIR% -source %SOURCEDIR%	-force30 
 
-%BUILD_SHADER% stdshader_invasion_dx9_30			-game %GAMEDIR% -source %SOURCEDIR% -dx9_30	-force30 
+%BUILD_SHADER% stdshader_invasion_dx9_30			-game %GAMEDIR% -source %SOURCEDIR%	-force30 
 
 
 rem echo.
